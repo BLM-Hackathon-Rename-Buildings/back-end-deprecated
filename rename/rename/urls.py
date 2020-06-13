ad250324f65c339f71324d2a7947b60af0041e8d
Natalie Jane Edson <nataliejaneedson@gmail.com>
2020-06-13 11:46:19 -0700

    gets api working

--- rename/rename/urls.py
+++ rename/rename/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from symbols import views
+
+
+router = routers.DefaultRouter()
+router.register(r'contacts', views.ContactViewSet)
+router.register(r'honorees', views.HonoreeViewSet)
+router.register(r'symbols', views.SymbolViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
